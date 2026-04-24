a7abe79f2834f01b2e7e1de58f8602272307b1e9
bastqq <bastqq@gmail.com>
2026-04-24 23:37:26 +0300

    feat-FLAB-12-homework-p2 initial setup

---
+++ lib/lesson_12/feat-FLAB-12-homework-p2.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WidgetsP2 extends StatefulWidget {
+  const WidgetsP2({super.key});
+
+  @override
+  State<WidgetsP2> createState() => _WidgetsP2State();
+}
+
+class _WidgetsP2State extends State<WidgetsP2> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
